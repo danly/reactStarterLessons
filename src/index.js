71846139525c1cae9d10ed6f1d0fb279d585db23
danlyg <gih.daniel@gmail.com>
2016-06-14 01:53:48 -0700

    Creates simple youtube with just react and yt-api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDHtTLKyJkFxrQp20ChR3f_Mv5Qkdqdvq0';
 
 // Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.videoSearch('casey neistat');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
 // Render the HTML and send it to the DOM
